87fb4ea7114abd4a45509e7f068deb22a155b51b
mmgulzaar <mmgulzaar@gmail.com>
2021-05-22 19:14:08 +0530

    Day6

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:learning_one/pages/home_page.dart';
 import 'package:learning_one/pages/login_page.dart';
+import 'package:learning_one/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,14 +18,15 @@ class MyApp extends StatelessWidget {
         fontFamily: GoogleFonts.lato().fontFamily,
         // primaryTextTheme: GoogleFonts.latoTextTheme()),
       ),
+      // debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
